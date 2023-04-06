3d937720f3760f6b8825e41b3a90ce6361a48902
anreh <anurehka@gmail.com>
2023-04-06 18:13:09 +0800

    Initial commit

---
+++ app/src/main/java/my/edu/tarc/contact/ContactDao.kt
@@ -0,0 +1,20 @@
+package my.tarc.mycontact
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+//Provides Database function = CRUD (Create, Retrieve, Update, Delete)
+@Dao
+interface ContactDao {
+    @Query("SELECT * FROM contact ORDER BY name ASC")
+    fun getAllContact(): LiveData<List<Contact>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(contact: Contact)
+
+    @Delete
+    suspend fun delete(contact: Contact)
+
+    @Update
+    suspend fun update(contact: Contact)
+}
